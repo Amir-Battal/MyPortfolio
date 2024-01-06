c6bca791c6f68a90dc313310b7d467518aa3b579
Amir Battal <amirbattal@Amirs-MacBook-Air.local>
2024-01-06 21:14:52 +0300

    initial

---
+++ README.md
@@ -0,0 +1,6 @@
+# AmirBattal
+A Responsive One Page Portfolio Website
+
+
+
+
---
+++ js/NewsLetter.js
@@ -0,0 +1,5 @@
+function ml_webform_success_10742456() {
+    var $ = ml_jQuery || jQuery;
+    $('.ml-subscribe-form-10742456 .row-success').show();
+    $('.ml-subscribe-form-10742456 .row-form').hide();
+}
